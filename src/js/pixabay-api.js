964661a3cc169d98386ceb808feecae0b0ff38ef
Oleksii <shcherbaalex77@gmail.com>
2024-03-27 05:01:08 -0400

    refactored to use axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,20 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
 const pixabayApi = (search_word, render_function) => {
-  const BASE_URL = 'https://pixabay.com/api/';
-  const request_params = new URLSearchParams({
+  const request_params = {
     key: '42955810-525eb75005697c7b81a0edb8d',
     q: search_word,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  });
+  };
 
-  fetch(BASE_URL + '?' + request_params.toString())
+  axios
+    .get('', { params: request_params })
     .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      return response.json();
-    })
-    .then(data => {
-      render_function(data);
+      render_function(response.data);
     })
     .catch(error => {
       console.log('Error:  ', error);
